40eb834ac540077c8f281088abd29e789fee28fb
PhoenixFairy <microsoft@inno-sp.com>
2016-05-13 22:17:51 +0800

    Fix Bug : 1.

--- SRC/result.php
+++ SRC/result.php
@@ -1,7 +1,7 @@
 <?php
 class result{
-    private $result;
-    private $error_reason;
+    public  $result;
+    public $error_reason;
     public static function __construct($result,$error_reason) {
         $this->result = $result;
         $this->error_reason = $error_reason;
