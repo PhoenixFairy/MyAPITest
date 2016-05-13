0a8813923444f60d45fb6ce5e3fb6194dff88bd1
PhoenixFairy <microsoft@inno-sp.com>
2016-05-13 21:15:44 +0800

    Add Web Page

---
+++ SRC/UserInit.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Multicraft Server System - Powered by Axoford12</title>
+</head>
+<body>
+<h1>Login Site</h1>
+<form action="request_createUser.php" method="post">
+<br>Username:
+<input type="text" name="username"><br />
+<br>Passwd:
+<input type="text" name="password">
+<br />
+<br>Key:
+<input type="text" name="key">
+<br />
+<input type="submit" name="login" value="login">
+</form>
+</body>
+</html>
